5450d56a80876ce07414b6f8c00d6463375a81a3
Alejandro Lopez <eniak23@gmail.com>
2019-03-11 17:42:40 -0500

    Adding the BattleConfig and prefabs

---
+++ Assets/Scripts/BattleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Unity.Entities;
+using UnityEngine;
+public struct BattleConfigData : IComponentData
+{
+    public Entity prefabRed;
+    public Entity prefabBlue;
+}
+
+public class BattleConfig : MonoBehaviour, IConvertGameObjectToEntity, IDeclareReferencedPrefabs
+{
+    public GameObject prefabRed;
+    public GameObject prefabBlue;
+    
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity , new BattleConfigData
+        {
+            prefabRed  = conversionSystem.GetPrimaryEntity(prefabRed),
+            prefabBlue = conversionSystem.GetPrimaryEntity(prefabBlue)
+        });
+    }    
+
+    public void DeclareReferencedPrefabs(List<GameObject> gameObjects)
+    {
+        gameObjects.Add(prefabRed);
+        gameObjects.Add(prefabBlue);
+    }
+}
